51d18ff4b5df18153ba8718a8487de63eabbe86f
pawan singh <kushpawal1@gmail.com>
2021-04-09 15:07:05 +0530

    [UC4] added Compute c + a / b logic code in airthmaticOperation.sh

--- airthmaticOperation.sh
+++ airthmaticOperation.sh
@@ -2,17 +2,22 @@
 
 echo "Enter the three number to perform airthmatic operation : "
 
-#UC1 add read code 
+#UC1 adding read code 
 read -p "Enter the First number : " a
 read -p "Enter the Second number : " b
 read -p "Enter the Third number : " c
 
-#UC2 add logic for Compute a + b * c
+#UC2 adding logic for Compute a + b * c
 
 result1=$(( a+b*c ))
 echo $result1
 
-#UC3 add logic for Compute a * b + c
+#UC3 adding logic for Compute a * b + c
 
 result2=$(( a*b+c ))
 echo $result2
+
+#UC4 adding logic for Compute c + a / b
+
+result3=$(( c+a/b ))
+echo $result3
